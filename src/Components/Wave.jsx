55ec0eb93f9fa58cb9a3b1be508b290b583d5d63
Anirudh A V <anirudh.av02@gmail.com>
2023-03-10 23:34:00 +0530

    feat: wave component styled

---
+++ src/Components/Wave.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Wave.css";
+
+const Wave = () => {
+  return (
+    <div className="loader-container">
+      <div className="rectangle-1"></div>
+      <div className="rectangle-2"></div>
+      <div className="rectangle-3"></div>
+      <div className="rectangle-4"></div>
+      <div className="rectangle-5"></div>
+      <div className="rectangle-6"></div>
+      <div className="rectangle-5"></div>
+      <div className="rectangle-4"></div>
+      <div className="rectangle-3"></div>
+      <div className="rectangle-2"></div>
+      <div className="rectangle-1"></div>
+    </div>
+  );
+};
+
+export default Wave;
