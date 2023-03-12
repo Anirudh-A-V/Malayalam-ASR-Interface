e3bfdc150d6a6c9cc509f9091f75d345140cf471
Anirudh A V <anirudh.av02@gmail.com>
2023-03-12 19:14:05 +0530

    feat: Title chaged

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,211 @@
-import { useContext, useRef } from "react"
-import { StateContext } from "./contexts/ContextProvider"
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import { BsFillPauseFill, BsStopFill, BsFillPlayFill } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+
+import "./index.css";
+import Wave from "./Components/Wave";
+import Ripple from "./Components/Ripple";
 
 export default function App() {
-  const { recording, setRecording, pause, setPause, audioBlob, setAudioBlob, recordings, setRecordings, uploaded, setUploaded } = useContext(StateContext)
+	const [isOpen, setIsOpen] = useState(false);
+	const [isPaused, setIsPaused] = useState(false);
+	const [recording, setRecording] = useState(false);
+	const [audioBlob, setAudioBlob] = useState(null);
+	const [uploaded, setUploaded] = useState(false);
+	const [convertedAudio, setConvertedAudio] = useState(null);
+	const [hasConverted, setHasConverted] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [hover, setHover] = useState(false);
 
-  const mediaRecorderRef = useRef(null);
+	const mediaRecorderRef = useRef(null);
 
-  const startRecording = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    mediaRecorderRef.current = new MediaRecorder(stream);
-    mediaRecorderRef.current.start();
+	const startRecording = async () => {
+		const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+		mediaRecorderRef.current = new MediaRecorder(stream);
+		mediaRecorderRef.current.start();
 
-    const chunks = [];
-    mediaRecorderRef.current.addEventListener("dataavailable", (event) => {
-      chunks.push(event.data);
-    });
+		const chunks = [];
+		mediaRecorderRef.current.addEventListener("dataavailable", (event) => {
+			chunks.push(event.data);
+		});
 
-    mediaRecorderRef.current.addEventListener("stop", () => {
-      setAudioBlob(new Blob(chunks));
-    });
+		mediaRecorderRef.current.addEventListener("stop", () => {
+			setAudioBlob(new Blob(chunks));
+		});
 
-    setRecording(true);
-    setUploaded(false);
-  };
+		setRecording(true);
+		setUploaded(false);
+	};
 
-  const pauseRecording = () => {
-    mediaRecorderRef.current.pause();
-    setPause(true);
-  };
+	const pauseRecording = () => {
+		mediaRecorderRef.current.pause();
+		setIsPaused(true);
+	};
 
-  const resumeRecording = () => {
-    mediaRecorderRef.current.resume();
-    setPause(false);
-  };
+	const resumeRecording = () => {
+		mediaRecorderRef.current.resume();
+		setIsPaused(false);
+	};
 
-  const stopRecording = () => {
-    mediaRecorderRef.current.stop();
-    setRecording(false);
+	const stopRecording = () => {
+		mediaRecorderRef.current.stop();
+		setRecording(false);
 
-  };
+		convertAudio()
+	};
 
-  return (
-    <>
-      <main className="container">
-        <h1>React Media Recorder</h1>
-        <div className="record-audio">
-          {recording && pause && <button className="record-resume" onClick={resumeRecording}>Resume Recording</button>}
-          {recording && !pause && <button className="record-pause" onClick={pauseRecording}>Pause Recording</button>}
-          {recording ? (
-            <>
-              <button className="record-stop" onClick={stopRecording}>Stop Recording</button>
-            </>
-          ) : (
-            <button className="record-start" onClick={startRecording}>Start Recording</button>
-          )}
-          {/* {audioBlob && !recording && !uploaded && <button className="record-upload" onClick={uploadAudio}>Upload Audio</button>} */}
-          {audioBlob && !recording && !uploaded && <audio controls src={URL.createObjectURL(audioBlob)} />}
-        </div>
-      </main>
-      {recordings.length > 0 &&
-        <div className="recorded-audios">
-          <h2>Recorded Audios</h2>
-          <div className="recorded-audios-list">
-            {recordings.map(recording => (
-              <div className="recorded-audio" key={recording.id}>
-                <audio controls src={recording.url} />
-                <FaTrash className="record-delete" onClick={() => deleteRecording(recording.id)}/>
-              </div>
-            ))}
-          </div>
-        </div>
-      }
-    </>
-  )
+	const convertAudio = async () => {
+		handleLoading(true)
+
+		const url = 'http://gokulgmenon.pythonanywhere.com/post';
+
+		const formData = new FormData();
+		formData.append('file', audioBlob);
+
+		const response = await fetch(url, {
+			method: 'POST',
+			body: formData
+		});
+
+		if (response.status >= 200 && response.status < 300) {
+			console.log('success')
+			setHasConverted(true)
+			const blob = await response.blob();
+			const blobURL = window.URL.createObjectURL(blob);
+			setLoading(false)
+			setConvertedAudio(blobURL)
+			// const link = document.createElement('a');
+			// link.href = blobURL;
+			// link.download = 'audio.mp3';
+			// link.click();
+
+		} else {
+			console.log(response)
+			setLoading(false)
+		}
+
+	}
+
+	const handleLoading = (load) => {
+		setTimeout(() => {
+			setLoading(load)
+		}, 700)
+	}
+
+	const handleHover = () => {
+		setTimeout(() => {
+			setHover(false)
+		}, 300)
+	}
+
+	return (
+		<>
+			<div className="flex justify-center items-center flex-col">
+				<div className=" mb-24 mt-14 flex flex-col items-center justify-center">
+					<div className="text-8xl text-transparent title font-bold font-outline-2">
+						Malayalam
+					</div>
+					<div className="title text-8xl text-[#F6F8FB] title font-bold">
+						XLSR Model
+					</div>
+				</div>
+				<Ripple on={loading}>
+					<motion.div
+						layout
+						data-isopen={isOpen}
+						initial={{ borderRadius: 50 }}
+						className="parent mt-12 cursor-pointer"
+					>
+						{/* <span class="loader"> */}
+						{isOpen ? (
+							<>
+								<Wave />
+								<motion.div className="buttons">
+									<motion.div
+										layout
+										className="child"
+										data-isopen={isOpen}
+										onClick={() => {
+											stopRecording()
+											setIsOpen(!isOpen)
+										}}
+									>
+										<BsStopFill className="controls stop" />
+									</motion.div>
+									{isPaused ? (
+										<motion.div
+											className="child"
+											data-isopen={isOpen}
+											onClick={resumeRecording}
+										>
+											<BsFillPlayFill className="controls pause" />
+										</motion.div>
+									) : (
+										<motion.div
+											className="child"
+											data-isopen={isOpen}
+											onClick={pauseRecording}
+										>
+											<BsFillPauseFill className="controls pause" />
+										</motion.div>
+									)}
+								</motion.div>
+							</>
+						) : (
+							<motion.div
+								layout
+								className="child"
+								data-isopen={isOpen}
+								onClick={() => {
+									startRecording()
+									setIsOpen(!isOpen)
+								}}
+							>
+								<div className="circle" />
+							</motion.div>
+						)}
+						{/* </span> */}
+					</motion.div>
+				</Ripple>
+				{/* {audioBlob && <audio controls src={URL.createObjectURL(audioBlob)} className="mt-10" />} */}
+
+				<audio controls src={convertedAudio} className="mt-10 hidden" autoPlay={true} />
+			</div>
+			<div className="details">
+				<div className="flex flex-col items-start justify-center">
+					<div className="text-base text-[#F6F8FB] title font-bold">
+						Research Paper by Kavya Suresh
+					</div>
+					<div className="text-sm text-[#F6F8FB] title font-bold">
+						Research Intern at IIT Madras
+					</div>
+					<div className="text-sm text-[#F6F8FB] title font-bold">
+						Gokul G Menon, Ashish Abraham
+					</div>
+				</div>
+			</div>
+			<motion.div className="floating cursor-pointer"
+				layout
+				transition={{ duration: 0.3, ease: "backOut" }}
+
+				data-onhover={hover}
+				initial={{ borderRadius: 50 }}
+				onMouseEnter={() => { setHover(true) }}
+				onMouseLeave={() => { handleHover() }}
+			>
+				{hover && <motion.p className="text-xl font-normal text-[#F6F8FB] ">Get the Research Paper</motion.p>}
+				<HiDownload className="icon text-4xl text-[#F6F8FB] cursor-pointer rounded-full p-1" onClick={() => { }} />
+				{/* {hover ? (
+					<>
+						<motion.p className="text-xl font-normal text-[#F6F8FB] ">Get the Research Paper</motion.p>
+						<HiDownload className="icon text-4xl text-[#F6F8FB] cursor-pointer rounded-full p-1" onClick={() => { }} />
+					</>) : (
+					<>
+						<HiDownload className="icon text-4xl text-[#F6F8FB] cursor-pointer rounded-full p-1" onClick={() => { }} />
+					
+				)} */}
+			</motion.div>
+		</>
+	);
 }
